6277917f1b90a122dbee35f6d26dccecb2af67e1
PabloFC <pablofc_68@hotmail.com>
2025-04-18 11:15:42 +0200

    docs.md added

---
+++ milestone2/push_swap/docs.md
@@ -0,0 +1,10 @@
+# Push Swap Resources
+
+## Blogs
+- [Push Swap: Efficient Positional Sorting Algorithm](https://web.archive.org/web/20220802162832/https://www.codequoi.com/en/push_swap-efficient-positional-sorting-algorithm/#sorting_algorithm_for_3_numbers)
+- [Push Swap in Less Than 4200 Operations](https://medium.com/@ulysse.gerkens/push-swap-in-less-than-4200-operations-c292f034f6c0)
+
+## YouTube Videos
+- [Push Swap Explained by Oceano](https://www.youtube.com/watch?app=desktop&v=OaG81sDEpVk&ab_channel=Oceano)
+- [Push Swap Explained by ChioCode](https://www.youtube.com/watch?v=W_euZjKoHkM&ab_channel=ChioCode)
+- [Push Swap in Short Format](https://www.youtube.com/shorts/4hJAqspcubU)
--- milestone2/push_swap/includes/push_swap.h
+++ milestone2/push_swap/includes/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:15:08 by pafuente          #+#    #+#             */
-/*   Updated: 2025/04/17 11:16:02 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/04/17 13:40:40 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,9 @@
 // Estructura para los nodos de la pila
 typedef struct s_stack
 {
-    int             value;          // Valor del número
-    int             index;          // Índice (para Radix Sort)
-    struct s_stack  *next;          // Puntero al siguiente nodo
+    int             value;         
+    int             index;          
+    struct s_stack  *next;          
 }                   t_stack;
 
 #endif
