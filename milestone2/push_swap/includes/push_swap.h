2363a7ad2d88999a5a5b8ff01473a040461f6324
PabloFC <pablofc_68@hotmail.com>
2025-04-29 11:20:51 +0200

    push.c commented

--- milestone2/push_swap/includes/push_swap.h
+++ milestone2/push_swap/includes/push_swap.h
@@ -6,77 +6,77 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/17 11:15:08 by pafuente          #+#    #+#             */
-/*   Updated: 2025/04/28 10:16:17 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/04/28 11:28:26 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#define PUSH_SWAP_H
+# define PUSH_SWAP_H
 
-#include <unistd.h>
-#include <stdlib.h>
-#include <limits.h>
-#include <stdbool.h>
-#include <stdarg.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+# include <stdbool.h>
+# include <stdarg.h>
 
 typedef struct s_stack_node
 {
-    int value;
-    int index;
-    struct s_stack_node *next;
-    struct s_stack_node *prev;
-} t_stack_node;
+	int					value;
+	int					index;
+	struct s_stack_node	*next;
+	struct s_stack_node	*prev;
+}	t_stack_node;
 
 /* ------------------- Operations ------------------- */
 /* Swap */
-void sa(t_stack_node **a, bool print);
-void sb(t_stack_node **b, bool print);
-void ss(t_stack_node **a, t_stack_node **b, bool print);
+void				sa(t_stack_node **a, bool print);
+void				sb(t_stack_node **b, bool print);
+void				ss(t_stack_node **a, t_stack_node **b, bool print);
 
 /* Push */
-void pa(t_stack_node **a, t_stack_node **b, bool print);
-void pb(t_stack_node **a, t_stack_node **b, bool print);
+void				pa(t_stack_node **a, t_stack_node **b, bool print);
+void				pb(t_stack_node **a, t_stack_node **b, bool print);
 
 /* Rotate */
-void ra(t_stack_node **a, bool print);
-void rb(t_stack_node **b, bool print);
-void rr(t_stack_node **a, t_stack_node **b, bool print);
+void				ra(t_stack_node **a, bool print);
+void				rb(t_stack_node **b, bool print);
+void				rr(t_stack_node **a, t_stack_node **b, bool print);
 
 /* Reverse Rotate */
-void rra(t_stack_node **a, bool print);
-void rrb(t_stack_node **b, bool print);
-void rrr(t_stack_node **a, t_stack_node **b, bool print);
+void				rra(t_stack_node **a, bool print);
+void				rrb(t_stack_node **b, bool print);
+void				rrr(t_stack_node **a, t_stack_node **b, bool print);
 
 /* ------------------- Parsing ------------------- */
-t_stack_node *parse_args(int argc, char **argv);
+t_stack_node		*parse_args(int argc, char **argv);
 
 /* ------------------- Sort Utils ------------------- */
-int find_max_index_pos(t_stack_node **stack);
-int find_min_index_pos(t_stack_node **stack);
-void move_to_top(t_stack_node **stack, int pos, bool is_stack_a);
-int get_max_index(t_stack_node *stack);
-int get_min_index(t_stack_node *stack);
+int					find_max_index_pos(t_stack_node **stack);
+int					find_min_index_pos(t_stack_node **stack);
+void				move_to_top(t_stack_node **stack, int pos, bool is_stack_a);
+int					get_max_index(t_stack_node *stack);
+int					get_min_index(t_stack_node *stack);
 
 /* ------------------- Utils ------------------- */
 /* Stack */
-int stack_size(t_stack_node *stack);
-bool is_sorted(t_stack_node *stack);
-void free_stack(t_stack_node **stack);
-t_stack_node *stack_new(int value);
-void stack_add_back(t_stack_node **stack, t_stack_node *new);
+int					stack_size(t_stack_node *stack);
+bool				is_sorted(t_stack_node *stack);
+void				free_stack(t_stack_node **stack);
+t_stack_node		*stack_new(int value);
+void				stack_add_back(t_stack_node **stack, t_stack_node *new);
 
 /* Validation */
-int ft_isdigit(int c);
-long ft_atol(const char *str);
-void exit_with_error(void);
+int					ft_isdigit(int c);
+long				ft_atol(const char *str);
+void				exit_with_error(void);
 
 /* Output */
-int ft_printf(const char *format, ...);
+int					ft_printf(const char *format, ...);
 
 /* ------------------- Algoritmos ------------------- */
-void sort_two(t_stack_node **a);
-void sort_three(t_stack_node **a);
-void sort_four_five(t_stack_node **a, t_stack_node **b);
-void chunk_sort(t_stack_node **a, t_stack_node **b);
+void				sort_two(t_stack_node **a);
+void				sort_three(t_stack_node **a);
+void				sort_four_five(t_stack_node **a, t_stack_node **b);
+void				chunk_sort(t_stack_node **a, t_stack_node **b);
 
 #endif
--- milestone2/push_swap/src/operations/push.c
+++ milestone2/push_swap/src/operations/push.c
@@ -6,50 +6,46 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:48:11 by pafuente          #+#    #+#             */
-/*   Updated: 2025/04/25 12:41:03 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/04/29 11:19:52 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void pa(t_stack_node **a, t_stack_node **b, bool print)
+// Moves the top node from stack 'b' to stack 'a'.
+void	pa(t_stack_node **a, t_stack_node **b, bool print)
 {
-    t_stack_node *tmp;
+	t_stack_node	*tmp;
 
-    if (!*b)
-        return;
-
-    tmp = *b;
-    *b = (*b)->next;
-    if (*b)
-        (*b)->prev = NULL;
-
-    tmp->next = *a;
-    if (*a)
-        (*a)->prev = tmp;
-    *a = tmp;
-
-    if (print)
-        write(1, "pa\n", 3);
+	if (!*b)
+		return ;
+	tmp = *b;
+	*b = (*b)->next;
+	if (*b)
+		(*b)->prev = NULL;
+	tmp->next = *a;
+	if (*a)
+		(*a)->prev = tmp;
+	*a = tmp;
+	if (print)
+		write(1, "pa\n", 3);
 }
 
-void pb(t_stack_node **a, t_stack_node **b, bool print)
+// Moves the top node from stack 'b' to stack 'a'.
+void	pb(t_stack_node **a, t_stack_node **b, bool print)
 {
-    t_stack_node *tmp;
+	t_stack_node	*tmp;
 
-    if (!*a)
-        return;
-
-    tmp = *a;
-    *a = (*a)->next;
-    if (*a)
-        (*a)->prev = NULL;
-
-    tmp->next = *b;
-    if (*b)
-        (*b)->prev = tmp;
-    *b = tmp;
-
-    if (print)
-        write(1, "pb\n", 3);
+	if (!*a)
+		return ;
+	tmp = *a;
+	*a = (*a)->next;
+	if (*a)
+		(*a)->prev = NULL;
+	tmp->next = *b;
+	if (*b)
+		(*b)->prev = tmp;
+	*b = tmp;
+	if (print)
+		write(1, "pb\n", 3);
 }
