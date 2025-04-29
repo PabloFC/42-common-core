4b0e8d59bb69f4b301ef7c83d08cf50110807b9c
PabloFC <pablofc_68@hotmail.com>
2025-04-29 11:53:31 +0200

    reverse rotate comented

--- milestone2/push_swap/src/operations/reverse_rotate.c
+++ milestone2/push_swap/src/operations/reverse_rotate.c
@@ -6,61 +6,63 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:58:17 by pafuente          #+#    #+#             */
-/*   Updated: 2025/04/25 12:45:30 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/04/29 11:52:47 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void rra(t_stack_node **a, bool print)
+//Performs a reverse rotation on a stack, moving the last element to the top.
+void	rra(t_stack_node **a, bool print)
 {
-    t_stack_node *last;
-    t_stack_node *second_last;
+	t_stack_node	*last;
+	t_stack_node	*second_last;
 
-    if (!*a || !(*a)->next)
-        return;
-    last = *a;
-    while (last->next)
-    {
-        second_last = last;
-        last = last->next;
-    }
-    second_last->next = NULL;
-    last->prev = NULL;
-    last->next = *a;
-    (*a)->prev = last;
-    *a = last;
-    if (print)
-        write(1, "rra\n", 4);
+	if (!*a || !(*a)->next)
+		return ;
+	last = *a;
+	while (last->next)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->prev = NULL;
+	last->next = *a;
+	(*a)->prev = last;
+	*a = last;
+	if (print)
+		write(1, "rra\n", 4);
 }
 
-void rrb(t_stack_node **b, bool print)
+//Performs a reverse rotation on b stack, moving the last element to the top.
+void	rrb(t_stack_node **b, bool print)
 {
-    t_stack_node *last;
-    t_stack_node *second_last;
+	t_stack_node	*last;
+	t_stack_node	*second_last;
 
-    if (!*b || !(*b)->next)
-        return;
-    last = *b;
-    while (last->next)
-    {
-        second_last = last;
-        last = last->next;
-    }
-    second_last->next = NULL;
-    last->prev = NULL;
-    last->next = *b;
-    (*b)->prev = last;
-    *b = last;
-    if (print)
-        write(1, "rrb\n", 4);
+	if (!*b || !(*b)->next)
+		return ;
+	last = *b;
+	while (last->next)
+	{
+		second_last = last;
+		last = last->next;
+	}
+	second_last->next = NULL;
+	last->prev = NULL;
+	last->next = *b;
+	(*b)->prev = last;
+	*b = last;
+	if (print)
+		write(1, "rrb\n", 4);
 }
 
-void rrr(t_stack_node **a, t_stack_node **b, bool print)
+//Performs a simultaneous reverse rotation on both stacks
+void	rrr(t_stack_node **a, t_stack_node **b, bool print)
 {
-    rra(a, false);
-    rrb(b, false);
-    if (print)
-        write(1, "rrr\n", 4);
-
+	rra(a, false);
+	rrb(b, false);
+	if (print)
+		write(1, "rrr\n", 4);
 }
