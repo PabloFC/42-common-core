c2929d775d3676fd56f3d6fe9627eaec1cd0556a
PabloFC <pablofc_68@hotmail.com>
2025-04-29 13:07:23 +0200

    rotate and swap commented

--- milestone2/push_swap/src/operations/rotate.c
+++ milestone2/push_swap/src/operations/rotate.c
@@ -6,56 +6,60 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:58:24 by pafuente          #+#    #+#             */
-/*   Updated: 2025/04/25 12:49:08 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/04/29 12:54:53 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void ra(t_stack_node **a, bool print)
+//move the first node of stack a to the end of the stack.
+void	ra(t_stack_node **a, bool print)
 {
-    t_stack_node *first;
-    t_stack_node *last;
+	t_stack_node	*first;
+	t_stack_node	*last;
 
-    if (!*a || !(*a)->next)
-        return;
-    first = *a;
-    last = *a;
-    while (last->next)
-        last = last->next;
-    *a = first->next;
-    (*a)->prev = NULL;
-    first->next = NULL;
-    last->next = first;
-    first->prev = last;
-    if (print)
-        write(1, "ra\n", 3);
+	if (!*a || !(*a)->next)
+		return ;
+	first = *a;
+	last = *a;
+	while (last->next)
+		last = last->next;
+	*a = first->next;
+	(*a)->prev = NULL;
+	first->next = NULL;
+	last->next = first;
+	first->prev = last;
+	if (print)
+		write(1, "ra\n", 3);
 }
 
-void rb(t_stack_node **b, bool print)
+//move the first node of stack a to the end of the stack.
+void	rb(t_stack_node **b, bool print)
 {
-    t_stack_node *first;
-    t_stack_node *last;
+	t_stack_node	*first;
+	t_stack_node	*last;
 
-    if (!*b || !(*b)->next)
-        return;
-    first = *b;
-    last = *b;
-    while (last->next)
-        last = last->next;
-    *b = first->next;
-    (*b)->prev = NULL;
-    first->next = NULL;
-    last->next = first;
-    first->prev = last;
-    if (print)
-        write(1, "rb\n", 3);
+	if (!*b || !(*b)->next)
+		return ;
+	first = *b;
+	last = *b;
+	while (last->next)
+		last = last->next;
+	*b = first->next;
+	(*b)->prev = NULL;
+	first->next = NULL;
+	last->next = first;
+	first->prev = last;
+	if (print)
+		write(1, "rb\n", 3);
 }
 
-void rr(t_stack_node **a, t_stack_node **b, bool print)
+// performs a simultaneous forward rotation on both stacks a and b,
+// moving the first node of each stack to the end.
+void	rr(t_stack_node **a, t_stack_node **b, bool print)
 {
-    ra(a, false);
-    rb(b, false);
-    if (print)
-        write(1, "rr\n", 3);
+	ra(a, false);
+	rb(b, false);
+	if (print)
+		write(1, "rr\n", 3);
 }
--- milestone2/push_swap/src/operations/swap.c
+++ milestone2/push_swap/src/operations/swap.c
@@ -6,57 +6,59 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 12:58:20 by pafuente          #+#    #+#             */
-/*   Updated: 2025/04/25 12:59:57 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/04/29 13:06:29 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void sa(t_stack_node **a, bool print)
+//The sa function swaps the first two nodes of stack a
+void	sa(t_stack_node **a, bool print)
 {
-    t_stack_node *first;
-    t_stack_node *second;
+	t_stack_node	*first;
+	t_stack_node	*second;
 
-    if (!*a || !(*a)->next)
-        return;
-    first = *a;
-    second = first->next;
-    first->next = second->next;
-    if (second->next)
-        second->next->prev = first;
-    second->prev = NULL;
-    second->next = first;
-    first->prev = second;
-    *a = second;
-    if (print)
-        write(1, "sa\n", 3);
-        
+	if (!*a || !(*a)->next)
+		return ;
+	first = *a;
+	second = first->next;
+	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	*a = second;
+	if (print)
+		write(1, "sa\n", 3);
 }
 
-void sb(t_stack_node **b, bool print)
+//The sa function swaps the first two nodes of stack b
+void	sb(t_stack_node **b, bool print)
 {
-    t_stack_node *first;
-    t_stack_node *second;
+	t_stack_node	*first;
+	t_stack_node	*second;
 
-    if (!*b || !(*b)->next)
-        return;
-    first = *b;
-    second = first->next;
-    first->next = second->next;
-    if (second->next)
-        second->next->prev = first;
-    second->prev = NULL;
-    second->next = first;
-    first->prev = second;
-    *b = second;
-    if (print)
-        ft_printf("sb\n");
+	if (!*b || !(*b)->next)
+		return ;
+	first = *b;
+	second = first->next;
+	first->next = second->next;
+	if (second->next)
+		second->next->prev = first;
+	second->prev = NULL;
+	second->next = first;
+	first->prev = second;
+	*b = second;
+	if (print)
+		ft_printf("sb\n");
 }
 
-void ss(t_stack_node **a, t_stack_node **b, bool print)
+//The ss function swaps the first two nodes of both stacks a and b
+void	ss(t_stack_node **a, t_stack_node **b, bool print)
 {
-    sa(a, false);
-    sb(b, false);
-    if (print)
-        write(1, "ss\n", 3);
+	sa(a, false);
+	sb(b, false);
+	if (print)
+		write(1, "ss\n", 3);
 }
