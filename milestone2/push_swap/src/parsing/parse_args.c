32615f227aa7745ae3acdd0b6ee6a15ca14f2004
PabloFC <pablofc_68@hotmail.com>
2025-04-30 10:46:39 +0200

    parsing commented

--- milestone2/push_swap/src/parsing/parse_args.c
+++ milestone2/push_swap/src/parsing/parse_args.c
@@ -6,85 +6,100 @@
 /*   By: pafuente <pafuente@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/23 10:16:02 by pafuente          #+#    #+#             */
-/*   Updated: 2025/04/24 13:18:58 by pafuente         ###   ########.fr       */
+/*   Updated: 2025/04/29 13:51:28 by pafuente         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int is_valid_number(const char *str)
+// Checks if a string represents a valid integer number.
+static int	is_valid_number(const char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    if (str[i] == '-' || str[i] == '+')
-        i++;
-    if (!str[i])
-        return (0);
-    while (str[i])
-    {
-        if (!ft_isdigit(str[i]))
-            return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	if (str[i] == '-' || str[i] == '+')
+		i++;
+	if (!str[i])
+		return (0);
+	while (str[i])
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
-static void check_duplicates(t_stack_node *stack)
+// Verifies that there are no duplicate values
+// in the stack and exits on error if found.
+static void	check_duplicates(t_stack_node *stack)
 {
-    t_stack_node *tmp;
-    t_stack_node *compare;
+	t_stack_node	*tmp;
+	t_stack_node	*compare;
 
-    tmp = stack;
-    while (tmp)
-    {
-        compare = tmp->next;
-        while (compare)
-        {
-            if (tmp->value == compare->value)
-                exit_with_error();
-            compare = compare->next;
-        }
-        tmp = tmp->next;
-    }
+	tmp = stack;
+	while (tmp)
+	{
+		compare = tmp->next;
+		while (compare)
+		{
+			if (tmp->value == compare->value)
+				exit_with_error();
+			compare = compare->next;
+		}
+		tmp = tmp->next;
+	}
 }
 
-void assign_indexes(t_stack_node *stack) {
-    t_stack_node *tmp = stack;
-    while (tmp) {
-        int index = 0;
-        t_stack_node *compare = stack;
-        while (compare) {
-            if (compare->value < tmp->value) index++;
-            compare = compare->next;
-        }
-        tmp->index = index;  
-        tmp = tmp->next;
-    }
+// Assigns an index to each node in the stack
+// based on its value's relative order.
+void	assign_indexes(t_stack_node *stack)
+{
+	t_stack_node	*tmp;
+	t_stack_node	*compare;
+	int				index;
+
+	tmp = stack;
+	while (tmp)
+	{
+		index = 0;
+		compare = stack;
+		while (compare)
+		{
+			if (compare->value < tmp->value)
+				index++;
+			compare = compare->next;
+		}
+		tmp->index = index;
+		tmp = tmp->next;
+	}
 }
 
-t_stack_node *parse_args(int argc, char **argv)
+// Parses command-line arguments into a stack, validates them,
+// checks for duplicates, assigns indexes, and returns the stack.
+t_stack_node	*parse_args(int argc, char **argv)
 {
-    t_stack_node *stack_a;
-    long num;
-    int i;
+	t_stack_node	*stack_a;
+	long			num;
+	int				i;
 
-    stack_a = NULL;
-    i = 1;
-    while (i < argc)
-    {
-        if (!is_valid_number(argv[i]))
-            exit_with_error();
-        num = ft_atol(argv[i]);
-        if (num > INT_MAX || num < INT_MIN)
-            exit_with_error();
-        if (i == 1)
-            stack_a = stack_new((int)num);
-        else
-            stack_add_back(&stack_a, stack_new((int)num));
-        i++;
-    }
-    check_duplicates(stack_a);
-    assign_indexes(stack_a);
-    return (stack_a);
+	stack_a = NULL;
+	i = 1;
+	while (i < argc)
+	{
+		if (!is_valid_number(argv[i]))
+			exit_with_error();
+		num = ft_atol(argv[i]);
+		if (num > INT_MAX || num < INT_MIN)
+			exit_with_error();
+		if (i == 1)
+			stack_a = stack_new((int)num);
+		else
+			stack_add_back(&stack_a, stack_new((int)num));
+		i++;
+	}
+	check_duplicates(stack_a);
+	assign_indexes(stack_a);
+	return (stack_a);
 }
